fa139843bd72027cabd1e7ebc9ccf918b8f79cd3
fernandagutierrez <80538121+fernandagutierrez@users.noreply.github.com>
2021-03-15 14:36:26 -0700

    Add files via upload

---
+++ ipynb.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Project Description
+
+# After listening to Jaime explain his project in class he gave me an idea to do something similar but kind of different. In E2 there was a question with the Chinese New Year and as a mix of Jaime's project and that question I got my project idea. My project will tell someone how compatible they are to another person based on both of the person's astrology sign. It will first tell you what your sign is, then the other persons sign. After that it will print what percentage of compatibility you have. 
+# 
+# 
+
+# ## Project Code
+# 
+# If it makes sense for your project, you can have code and outputs here in the notebook as well.
+
+# In[1]:
+
+
+from my_module.functions import *
+
+
+#  ### Chat Bot
+# The start_the_chat function begins the chatbot.
+# 
+
+# In[2]:
+
+
+start_the_chat()
+
+
+# #### Extra Credit (*optional*)
+# 
+# Replace all of this text with a brief explanation (~3 sentences) of: 
+# 1. Your Python Background
+# 2. How your project went above and beyond the requirements of the project and/or how you challenged yourself to learn something new with the final project
+
+# In[ ]:
+
+
+
+
